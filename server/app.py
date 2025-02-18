6288a9edf555224ab587bfd960c31f23db74f6af
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-02-17 22:26:23 -0800

    Add Features in app.py file

--- server/app.py
+++ server/app.py
@@ -8,16 +8,51 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        image_url = request.get_json()['image_url']
+        bio = request.get_json()['bio']
+
+        new_user = User(
+            username = username,
+            image_url = image_url,
+            bio = bio
+        )
+        new_user.password_hash = request.get_json()['password']
+
+        if not new_user.username or not new_user.password_hash:
+            return {'error': 'User must have username and password.'}, 422
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        return new_user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 class RecipeIndex(Resource):
     pass
